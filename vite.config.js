f2c28d0ce988a8c99168f7db6c313d16f7db5cba
matheussantos-web <matheus281197@gmail.com>
2026-08-28 21:18:29 -0300

    Corrigir base do Vite para GitHub Pages (caminhos relativos)

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
+// base relativo para funcionar sob o subpath do GitHub Pages
+// (https://matheussantos-web.github.io/brokencrownsite/)
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: './',
 })
